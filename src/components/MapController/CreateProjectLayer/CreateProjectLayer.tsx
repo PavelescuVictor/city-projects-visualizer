06307840a17dd161c0e5ee8adeae9e3ecade929d
Victor Pavelescu <victor.pavelescu@endava.com>
2026-07-07 14:54:20 +0300

    Revert "Add striped polygon fills"
    This reverts commit 9d913dab0be9f7dc46280f82d60df9a863a0e718.

--- src/components/MapController/CreateProjectLayer/CreateProjectLayer.tsx
+++ src/components/MapController/CreateProjectLayer/CreateProjectLayer.tsx
@@ -6,7 +6,6 @@ import { useProjectEditing } from "../../../contexts";
 import { PROJECT_TYPES } from "../../../data/projects";
 import type { CreateProjectDraft, LngLat, ProjectType } from "../../../data/projects.types";
 import { polygonToLatLngs, toLatLng } from "../../../utils/geo";
-import { applyPolygonStripeFill } from "../../../utils/leafletPatterns";
 import { MarkerIcon } from "../MarkerIcon";
 import type { CreateProjectLayerProps } from "./CreateProjectLayer.types";
 
@@ -140,7 +139,6 @@ const CreateProjectLayer = (props: CreateProjectLayerProps) => {
 			opacity: 0.98,
 			weight: 4,
 		}).addTo(layerGroup);
-		applyPolygonStripeFill(polygon, CREATE_PROJECT_STYLE.color);
 
 		const commitRingChange = (nextRing: LngLat[]) => {
 			const committedRing = cloneRing(nextRing);
